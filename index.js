2c309f98f512f44511b7c73f332799679f8f4cb2
Tonmoy Roy <rxdevs100@gmail.com>
2021-10-30 23:25:07 +0600

    simple mongodb

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-// const port = process.env.PORT || 6000;
-const port = 7000;
+const port = process.env.PORT || 6000;
+// const port = 7000;
 
+// middleware setUp 
 app.use(cors());
 app.use(express.json());
 
@@ -17,8 +19,23 @@ async function run(){
     try {
         await client.connect();
         // console.log('database is connected');
-       /*  const database = client.db('travelExpress');
-        const bookingsCollection = database.collection('bookings'); */
+        const database = client.db('travelExpress');
+        const bookingPlacesCollection = database.collection('bookingPlaces');
+
+        // GET API
+        app.get('/booking_places', async (req, res) => {
+            const cursor = bookingPlacesCollection.find({});
+            const bookingPlaces = await cursor.toArray();
+            res.send(bookingPlaces);
+        });
+
+        // GET Single BOOKING API
+        app.get('/booking_places/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const bookingPlace = await bookingPlacesCollection.findOne(query);
+            res.json(bookingPlace);
+        })
     }
     finally {
         // await client.close();
