8b8688d9fb5648eef5a9dd04b8f87db34daa073e
Tonmoy Roy <rxdevs100@gmail.com>
2024-06-06 12:56:14 +0600

    chore: Update deps & fix errors

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const app = express();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
+const SSLCommerzPayment = require('sslcommerz');
+const port = process.env.PORT || 5000;
+
+
 require('dotenv').config();
-
-const app = express();
-const port = process.env.PORT || 7000;
-// const port = 7000;
-
 // middleware setUp 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@cluster0.dn7ou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -75,6 +76,78 @@ async function run(){
             });
             res.send(result);
         });
+
+        
+        // SSLCOMMERZ payment system initiation API
+        app.post('/init', (req, res) => {
+            // console.log(req.body);
+            const data = {
+                total_amount: req.body.total_amount,
+                currency: 'BDT',
+                tran_id: 'REF123',
+                success_url: 'http://localhost:5000/success',
+                fail_url: 'http://localhost:5000/fail',
+                cancel_url: 'http://localhost:5000/cancel',
+                ipn_url: 'http://localhost:5000/ipn',
+                shipping_method: 'Courier',
+                product_name: req.body.product_name,
+                product_image: req.body.product_image,
+                product_category: 'Electronic',
+                product_profile: req.body.product_profile,
+                cus_name: req.body.cus_name,
+                cus_email: req.body.cus_email,
+                cus_add1: 'Dhaka',
+                cus_add2: 'Dhaka',
+                cus_city: 'Dhaka',
+                cus_state: 'Dhaka',
+                cus_postcode: '1000',
+                cus_country: 'Bangladesh',
+                cus_phone: '01711111111',
+                cus_fax: '01711111111',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: 1000,
+                ship_country: 'Bangladesh',
+                multi_card_name: 'mastercard',
+                value_a: 'ref001_A',
+                value_b: 'ref002_B',
+                value_c: 'ref003_C',
+                value_d: 'ref004_D'
+            };
+            // console.log(data)
+            const sslcommer = new SSLCommerzPayment(process.env.STORE_ID,process.env.STORE_PASS,false) //true for live default false for sandbox
+            sslcommer.init(data).then(data => {
+                // process the response that got from sslcommerz
+                // console.log(data);
+
+                if (info.GatewayPageURL) {
+                    res.json(info.GatewayPageURL)
+                }
+                else {
+                    return res.status(400).json({
+                        message: "SSL session was not successful"
+                    })
+                }
+            });
+        });
+
+        app.post('/success', async (req, res) => {
+            console.log(req.body);
+            res.status(200).json(req.body);
+        });
+        app.post('/fail', async (req, res) => {
+            console.log(req.body);
+            res.status(400).json(req.body);
+        });
+        app.post('/cancel', async (req, res) => {
+            console.log(req.body);
+            res.status(200).json(req.body);
+        });
+
+
     }
     finally {
         // await client.close();
